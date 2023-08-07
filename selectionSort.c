f4e95092ccb15a4dcbbb9b81bf313d0d32aa3d94
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2023-08-07 21:43:08 +0530

    Add files via upload

---
+++ selectionSort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void selectionSort(int *A, int n)
+{
+    int indexOfMin, temp;
+    printf("Running Selection sort...\n");
+    for (int i = 0; i < n-1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(A[j] < A[indexOfMin])
+            {
+                indexOfMin = j;
+            }
+        }
+        //  Swap A[i] and A[indexOfMin]
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp; 
+    }
+}
+
+int main()
+{
+    // There will be total (n-1) passes and Max swaps = (n-1)
+    // Time complexity = O(n^2)
+    // Selection Sort is not Stable and is not adaptive
+    int A[] = {3, 5, 2, 13, 12};
+    int n = 5;
+    printArray(A, n);
+    selectionSort(A, n);
+    printArray(A, n);
+    return 0;
+}
