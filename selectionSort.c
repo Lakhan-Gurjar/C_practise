079b65442230e564d5cf7ce439cec48d19aca722
Lakhan Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-07-02 21:41:01 +0530

    Update selectionSort.c

--- selectionSort.c
+++ selectionSort.c
@@ -37,8 +37,8 @@ int main()
     // Selection Sort is not Stable and is not adaptive
     int A[] = {3, 5, 2, 13, 12};
     int n = 5;
-    // printArray(A, n);
-    // selectionSort(A, n);
-    // printArray(A, n);
+    printArray(A, n);
+    selectionSort(A, n);
+    printArray(A, n);
     return 0;
 }
