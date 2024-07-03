a21e06d1d909ed2075345d21f9a2684d3e18ccdf
Lakhan Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-07-03 12:29:13 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -36,9 +36,9 @@ int main()
 {
     int A[] = {12, 54, 65, 7, 23, 9};
     // int A[] = {1, 2, 3, 4, 5, 6};
-    int n = 6;
-    printArray(A, n); // Printing the array before sorting
-    bubbleSort(A, n); // Function to sort the array
-    printArray(A, n); // Printing the array after sorting
+    // int n = 6;
+    // printArray(A, n); // Printing the array before sorting
+    // bubbleSort(A, n); // Function to sort the array
+    // printArray(A, n); // Printing the array after sorting
     return 0;
 }
